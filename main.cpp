609dbbcd6b0967b27c9fe7040c68df486b39769f
Gamer2012 <65019724+Gamer2012@users.noreply.github.com>
2022-04-28 12:46:18 +0200

    first step

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <stdio.h>
 
 using std::cout;
 using std::cin;
@@ -6,5 +7,22 @@ using std::endl;
 
 int main() {
 	cout << "test" << endl;
+	int result;
+	char oldname[] = "oldname.cpp";
+	char newname[] = "newname.cpp";
+	
+	cout << "oldname: ";
+	cin >> oldname;
+
+	cout << "newname: ";
+	cin >> newname;
+
+	result = rename(oldname, newname);
+	if (result == 0 ) {
+		cout << "success";
+	}
+	else {
+		perror("Error: ");
+	}
 	return 0;
 }
